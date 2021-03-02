1122965bfa00b4e28aca217f1748de236cd6492a
ResadMemmedov0035 <77126746+ResadMemmedov0035@users.noreply.github.com>
2021-03-02 22:14:53 +0400

    Lesson_1 has added

---
+++ C# Console/Lesson_1/Lesson_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            Console.WriteLine(keyInfo.Key);
+            
+
+            int a = Console.Read();
+
+            Console.WriteLine(a);
+
+            /// yaxsi code ucun temeller, I guess ///
+            //
+            // readable
+            // good logic
+            // universal
+            // updateable
+
+            Console.Write("Adinizi daxil edin: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Yasinizi daxil edin: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(name + " " + age);
+
+            //Console.WriteLine(); // cw tab+tab
+
+            Console.WriteLine("Click to contunie");
+            Console.ReadKey(true);
+
+            */
+        }
+    }
+}
