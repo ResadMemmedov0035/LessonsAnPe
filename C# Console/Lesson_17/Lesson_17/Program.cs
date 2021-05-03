7e53fac8ca85b34e7dc26feb60fdf4bd33877597
ResadMemmedov0035 <77126746+ResadMemmedov0035@users.noreply.github.com>
2021-05-03 23:00:44 +0400

    Lesson_17 has been added

---
+++ C# Console/Lesson_17/Lesson_17/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Lesson_17
+{
+    class MyClass : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Done");
+        }
+    }
+
+    
+    public class Person
+    {
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString() =>
+            $"{Name}, {Age}";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* XML Serilazition
+             *      requires:
+             *      - public class
+             *      - parameterless ctor
+             * *
+
+            var xmlSerializer = new XmlSerializer(typeof(Person));
+
+            using (var fstream = new FileStream("person.xml", FileMode.Open))
+            {
+                var person = xmlSerializer.Deserialize(fstream) as Person;
+                Console.WriteLine(person);
+            };
+
+            //var person = new Person("Perviz", 21);
+
+            //using (var fstream = new FileStream("person.xml", FileMode.Create)) 
+            //{
+            //    xmlSerializer.Serialize(fstream, person);
+            //}
+
+
+
+            /* Binary Serilazition
+             *      requires:
+             *      - [Serializable]
+             * *
+
+            var formatter = new BinaryFormatter();
+
+            var people = new List<Person>
+            {
+                new Person{ Name = "Resad", Age = 17},
+                new Person{ Name = "Rasim", Age = 20},
+                new Person{ Name = "Perviz", Age = 21},
+            };
+
+            using (var fstream = new FileStream("people.bin", FileMode.Create))
+            {
+                formatter.Serialize(fstream, people);
+            }
+
+
+            /*
+
+            var person = new Person { Age = 21, Name = "Perviz" };
+
+            using (var fstream = new FileStream("person.bin", FileMode.Open))
+            {
+                var person1 = formatter.Deserialize(fstream) as Person;
+                Console.WriteLine(person1);
+            }
+
+            //using (var fstream = new FileStream("person.bin", FileMode.Create))
+            //{
+            //    formatter.Serialize(fstream, person);
+            //}
+
+
+
+
+            /*
+             
+
+            using (var fstream = new FileStream("somelines.txt", FileMode.Open))
+            {
+                using (var sreader = new StreamReader(fstream))
+                {
+                    while (!sreader.EndOfStream)
+                    {
+                        var str = sreader.ReadLine();
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+
+            using (var fstream = new FileStream("somelines.txt", FileMode.Create))
+            {
+                using (var swriter = new StreamWriter(fstream))
+                {
+                    var strs = new string[] { "Hi", "My", "Name", "Is", "Resad" };
+
+                    foreach (var item in strs)
+                    {
+                        swriter.WriteLine(item);
+                    }
+                }
+            }
+
+
+            using (var fstream = new FileStream("sometext.txt", FileMode.Open))
+            {
+                using (var sreader = new StreamReader(fstream))
+                {
+                    var str = sreader.ReadToEnd();
+                    Console.WriteLine(str);
+                }
+            }
+
+            using (var fstream = new FileStream("sometext.txt", FileMode.Create))
+            {
+                using (var swriter = new StreamWriter(fstream))
+                {
+                    swriter.Write("Hello");
+                }
+            }
+
+
+
+
+            //using (var fstream = new FileStream("sometext.txt", FileMode.Open))
+            //{
+            //    byte[] bts = new byte[50];
+            //    fstream.Read(bts, 0, bts.Length);
+            //    var str = Encoding.UTF8.GetString(bts);
+            //    Console.WriteLine(str);
+            //}
+
+
+            /*
+
+            //using (var my = new MyClass())
+            //{
+            //    Console.WriteLine("Ok");
+            //}
+
+            /*
+
+            //using (var fstream = new FileStream("sometext.txt", FileMode.Create))
+            //{
+            //    var str = "New Info";
+            //    var bts = Encoding.UTF8.GetBytes(str);
+            //    fstream.Write(bts, 0, bts.Length);
+            //}
+
+            //FileStream fstream = new FileStream("sometext.txt", FileMode.Append);
+
+            //var str = "New Info";
+            //var bts = Encoding.UTF8.GetBytes(str);
+            //fstream.Write(bts, 0, bts.Length);
+            //fstream.Close();
+
+
+
+            /*
+
+            var strs = File.ReadAllLines("testlines.txt");
+            foreach (var s in strs)
+            {
+                Console.WriteLine(s);
+            }
+
+            var str = File.ReadAllText("test.txt");
+            Console.WriteLine(str);
+
+            /*
+             
+            var strs = new string[] { "Hi", "My", "Name", "Is", "Resad" };
+
+            File.WriteAllLines("testlines.txt", strs);
+
+
+            File.WriteAllText(@"../../../mytext.txt", "Hello Perviz");
+
+            File.WriteAllText(@"C:\Users\hp\Desktop\test.txt", "Hello World");
+
+            File.WriteAllText("test.txt", "Hello World");
+
+            */
+        }
+    }
+}
